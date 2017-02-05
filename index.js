8663564c5fc1d33d2e8759de623967f988bd327c
sivankar <sivankarjain@gmail.com>
2017-02-05 16:22:45 +0530

    crawl medium.com without using async library, output is in out.csv

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var utils = require('./lib/utils');
-
-
+var url = require('url');
 
 var options = {
     hostname: 'medium.com',
@@ -9,10 +8,64 @@ var options = {
     method: 'GET'
 };
 
-utils.collectAllLinks(options, function(err, res){
-    if(err){
+var links = new Set();
+var iterator = null;
+
+Set.prototype.union = function(setB) {
+    var union = new Set(this);
+    for (var elem of setB) {
+        union.add(elem);
+    }
+    return union;
+}
+
+utils.collectAllLinks(options, function (err, res) {
+    if (err) {
         console.log(err);
-    }else{
+    } else {
+        links = res;
+        iterator = links[Symbol.iterator]();
         utils.writeLinksInCSVFormat('out.csv', res);
+        a(0);
     }
 });
+
+function a(count) {
+
+    this.count = count;
+
+    while (this.count < 5) {
+
+        data = iterator.next()
+        if (data.value) {
+            makeRequest(this, data.value);
+        } else {
+            console.log('Process complete!!');
+            break;
+        }
+
+    }
+}
+
+function makeRequest(self, data) {
+
+    var myurl = url.parse(data);
+    var options = {
+        hostname: 'medium.com',
+        port: 443,
+        path: myurl.pathname,
+        method: 'GET'
+    };
+
+    self.count++;
+    utils.collectAllLinks(options, function (err, res) {
+        if (err) {
+            console.log(err);
+        } else {
+//            console.log('--type of links---',links instanceof Set);
+            links.union(res);
+            utils.writeLinksInCSVFormat('out.csv', res);
+            a(--self.count);
+        }
+    });
+}
